edcb259c32b956fb46359a3795ae3d654202825c
Madhu Narasimhan <madhu_narasimhan@hotmail.com>
2018-06-05 21:49:48 -0700

    Function to find second largest element in BST

--- utils/tnode_int_test.c
+++ utils/tnode_int_test.c
@@ -35,6 +35,74 @@ delete_tnode(
 
 } /* end delete_tnode */
 
+int
+get_largest(
+	TNodeInt	*tnode_int
+)
+{
+	int result = 0;
+
+	if (tnode_int->right) {
+		result = get_largest(tnode_int->right);
+	}
+	else {
+		result = tnode_int->data;
+	}
+
+	return result;
+
+} /* end get_largest */
+
+int
+get_second_largest(
+	TNodeInt	*tnode_int,
+	int			parent_data
+)
+{
+	int result = 0;
+
+	if (tnode_int->right != NULL) {
+		result = get_second_largest(tnode_int->right, tnode_int->data);
+	}
+	else if (tnode_int->left != NULL && parent_data == -1) {
+		result = get_largest(tnode_int->left);
+	}
+	else {
+		result = parent_data;
+	}
+
+	return result;
+
+} /* end get_second_largest */
+
+/******************************************************************************
+ * Prints the second largest element of the binary search tree
+ *****************************************************************************/
+void
+print_second_largest(
+	TNodeInt	*tnode_int
+)
+{
+	int		result = -1;
+
+	if (tnode_int == NULL) {
+		printf("Empty tree\n");
+	}
+	else {
+		result = get_second_largest(tnode_int, -1);
+
+		if (result == -1) {
+			printf("Tree has a single node with value %d\n", tnode_int->data);
+		}
+		else {
+			printf("Tree has second largest value %d\n", result);
+		}
+	}
+
+	return;
+
+} /* end print_second_largest */
+
 /******************************************************************************
  * Driver for testing TreeNodeInt API
  *****************************************************************************/
@@ -61,6 +129,7 @@ main(
 	/* Testing printing empty tree */
 	printf("Integers in tree in-order\n");
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode_int_insert(tnode1, 3);
@@ -75,49 +144,61 @@ main(
 
 	printf("Integers in tree in-order\n");
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 32);
 
 	tnode1 = delete_tnode(tnode1, 33);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 140);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 79);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 3535);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 30);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 27);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 1);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 389);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 27);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
 
 	tnode1 = delete_tnode(tnode1, 3);
 	tnode_int_print(tnode1, TRAVERSAL_IN_ORDER);
+	print_second_largest(tnode1);
 	printf("Height of tree: %d\n", tnode_int_height(tnode1));
+
 /*
 	printf("Finding 27\n");
 	found_tnode = tnode_int_find(tnode1, 27);
